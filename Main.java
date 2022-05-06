da245a6e22dccf5a449a9c6e09fc75acce7af7a0
bluhhar <61960650+bluhhar@users.noreply.github.com>
2022-05-06 05:51:23 +0300

    UP 06.05.2022 FINAL VERSION
    +Рефакторинг
    +Добавлен выбор тестов + пользовательский ввод

--- Main.java
+++ Main.java
@@ -1,57 +1,65 @@
 package com.integral;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void SetFunction(String method){
-        SearchIntegral search = new SearchIntegral();
-        long start = System.currentTimeMillis();
-        //System.out.println("\nМетод левых прямоугольников: \n" + "Интеграл = " + search.method(0.5, 1.2, 52));
-        long finish = System.currentTimeMillis();
-        System.out.println("\nВремя выполнения алгоритма " + (finish - start) + " (мс)");
-    }
-
     public static void main(String[] args) {
-        Test1();
-        //Test2();
+        Scanner in = new Scanner(System.in);
+        System.out.println("""
+                Лабораторная работа №3: Вычисление интеграла
+                Функция: (2 * x + 0.5) / sin(x)
+                a = 0.5, b = 1.2
+                """);
+        System.out.println("Введите номер теста:");
+        int number = in.nextInt();
+
+        switch (number) {
+            case 1 -> {
+                System.out.println("Тест 1: Свободный выбор количества элементарных отрезков: ");
+                int n = in.nextInt();
+                Test0(n);
+            }
+            case 2 -> {
+                System.out.println("Тест 2: n = 10");
+                Test0(10);
+            }
+            case 3 -> {
+                System.out.println("Тест 3: n = 100");
+                Test0(100);
+            }
+            default ->
+                    System.out.println("Error: Введен номер несуществующего теста!");
+        }
+        in.close();
     }
 
-    public static void Test0(){
-
-    }
-
-    public static void Test1(){ //с 10 до 100 в отчет
-        System.out.println("Лабораторная работа №3: Вычисление интеграла" +
-                "\n Функция: \n");
-
+    public static void Test0(int n){
         SearchIntegral search = new SearchIntegral();
 
-        long start = System.currentTimeMillis();
-        System.out.println("\nМетод левых прямоугольников: \n" + "Интеграл = " + search.LeftRectangle(0.5, 1.2, 10));
-        long finish = System.currentTimeMillis();
-        System.out.println("\nВремя выполнения алгоритма " + (finish - start) + " (мс)");
+        long start = System.nanoTime();
+        System.out.println("\nМетод левых прямоугольников: \n" + "Интеграл = " + search.LeftRectangle(0.5, 1.2, n));
+        long finish = System.nanoTime();
+        System.out.println("Время выполнения алгоритма " + (finish - start) / 1000 + " (мс)");
 
-        start = System.currentTimeMillis();
-        System.out.println("\nМетод правых прямоугольников: \n" + "Интеграл = " + search.RightRectangle(0.5, 1.2, 10));
-        finish = System.currentTimeMillis();
-        System.out.println("\nВремя выполнения алгоритма " + (finish - start)+ " (мс)");
+        start = System.nanoTime();
+        System.out.println("\nМетод правых прямоугольников: \n" + "Интеграл = " + search.RightRectangle(0.5, 1.2, n));
+        finish = System.nanoTime();
+        System.out.println("Время выполнения алгоритма " + (finish - start) / 1000 + " (мс)");
 
-        start = System.currentTimeMillis();
-        System.out.println("\nМетод центральных прямоугольников: \n" + "Интеграл = " + search.CenterRectangle(0.5, 1.2, 10));
-        finish = System.currentTimeMillis();
-        System.out.println("\nВремя выполнения алгоритма " + (finish - start) + " (мс)");
+        start = System.nanoTime();
+        System.out.println("\nМетод центральных прямоугольников: \n" + "Интеграл = " + search.CenterRectangle(0.5, 1.2, n));
+        finish = System.nanoTime();
+        System.out.println("Время выполнения алгоритма " + (finish - start) / 1000 + " (мс)");
 
-        start = System.currentTimeMillis();
-        System.out.println("Метод трапеции: \n" + "Интеграл = " + search.Trapezoid(0.5, 1.2, 10));
-        finish = System.currentTimeMillis();
-        System.out.println("\nВремя выполнения алгоритма " + (finish - start) + " (мс)");
+        start = System.nanoTime();
+        System.out.println("\nМетод трапеции: \n" + "Интеграл = " + search.Trapezoid(0.5, 1.2, n));
+        finish = System.nanoTime();
+        System.out.println("Время выполнения алгоритма " + (finish - start) / 1000 + " (мс)");
 
-        start = System.currentTimeMillis();
-        System.out.println("\nПравило Симпсона: \n" + "Интеграл = " + search.Simpson(0.5, 1.2, 10));
-        finish = System.currentTimeMillis();
-        System.out.println("\nВремя выполнения алгоритма " + (finish - start) + " (мс)");
-    }
-
-    public static void Test2(){
-
+        start = System.nanoTime();
+        System.out.println("\nПравило Симпсона: \n" + "Интеграл = " + search.Simpson(0.5, 1.2, n));
+        finish = System.nanoTime();
+        System.out.println("Время выполнения алгоритма " + (finish - start) / 1000 + " (мс)");
     }
 }
